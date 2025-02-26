170994440f4b851c924ba8e1aed74f6ff2879fd9
TECH TO AM <105159281+TECHTOAMYT@users.noreply.github.com>
2025-02-26 16:13:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const dice = document.getElementById('dice');
+const message = document.getElementById('message');
+const saveButton = document.getElementById('save');
+const loadButton = document.getElementById('load');
+const diceSound = document.getElementById('diceSound');
+const moveSound = document.getElementById('moveSound');
+const background
